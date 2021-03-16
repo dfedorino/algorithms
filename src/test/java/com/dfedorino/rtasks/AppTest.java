769134e28d65b94c2dfa8a18bc85072cae64308a
Dmitry <dfedorino@gmail.com>
2021-03-16 18:08:14 +0800

    Initial commit

---
+++ src/test/java/com/dfedorino/rtasks/AppTest.java
@@ -0,0 +1,12 @@
+package com.dfedorino.rtasks;
+
+import static org.testng.Assert.assertTrue;
+
+import org.testng.annotations.Test;
+
+public class AppTest {
+    @Test
+    public void shouldAnswerWithTrue() {
+        assertTrue(true);
+    }
+}
