c285e8ff0ec1e763350925146c10941423a81e7c
Dmitry <dfedorino@gmail.com>
2021-03-16 21:18:18 +0800

    Add helper method, tests and method descriptions

--- src/main/java/com/dfedorino/rtasks/first_task/MachineDistributor.java
+++ src/main/java/com/dfedorino/rtasks/first_task/MachineDistributor.java
@@ -1,5 +1,7 @@
 package com.dfedorino.rtasks.first_task;
 
+import java.util.Objects;
+
 public class MachineDistributor {
     private final int ramPerMachine;
 
@@ -7,6 +9,29 @@ public class MachineDistributor {
         this.ramPerMachine = checkRam(ramPerMachine);
     }
 
+    /**
+     * Searches for the first element that is larger than the ramPerMachine value to exclude processes
+     * with unsupported sizes from the search range.
+     * @param sortedProcessArray - SORTED array with process ram consumption values
+     * @throws IllegalArgumentException if null is passed or if the given array contains negative values
+     * @return the first index of the unsupported element, otherwise -1
+     */
+    protected int getFirstUnsupportedProcessIndex(int[] sortedProcessArray) {
+        Objects.requireNonNull(sortedProcessArray);
+        for (int processIndex = 0; processIndex < sortedProcessArray.length; processIndex++) {
+            if (checkRam(sortedProcessArray[processIndex]) > ramPerMachine) {
+                return processIndex;
+            }
+        }
+        return -1;
+    }
+
+    /**
+     * Simple RAM value check
+     * @param ramPerMachine - any int value that is supposed to represent RAM amount
+     * @throws IllegalArgumentException when the given value does not satisfy conditions of a valid RAM amount
+     * @return argument value
+     */
     private int checkRam(int ramPerMachine) {
         if (ramPerMachine < 0) {
             throw new IllegalArgumentException("Given RAM amount is negative");
--- src/test/java/com/dfedorino/rtasks/first_task/MachineDistributorTest.java
+++ src/test/java/com/dfedorino/rtasks/first_task/MachineDistributorTest.java
@@ -1,8 +1,10 @@
 package com.dfedorino.rtasks.first_task;
 
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
-import static org.testng.Assert.*;
+import static org.testng.Assert.assertSame;
+import static org.testng.Assert.assertEquals;
 
 public class MachineDistributorTest {
     @Test
@@ -15,6 +17,13 @@ public class MachineDistributorTest {
         assertSame(md2, md2);
     }
 
+    @Test(dataProvider = "data-provider-getFirstUnsupportedProcessIndex")
+    public void getFirstUnsupportedProcessIndexTest(int[] sortedArray, int expectedIndex) {
+        MachineDistributor md = new MachineDistributor(16);
+        int actualIndex = md.getFirstUnsupportedProcessIndex(sortedArray);
+        assertEquals(actualIndex, expectedIndex);
+    }
+
     @Test(
             expectedExceptions = IllegalArgumentException.class,
             expectedExceptionsMessageRegExp = "Given RAM amount is negative"
@@ -22,5 +31,35 @@ public class MachineDistributorTest {
     public void checkRamTest() {
         MachineDistributor md = new MachineDistributor(-16);
         MachineDistributor md1 = new MachineDistributor(Integer.MIN_VALUE);
+    }
+
+    @Test(
+            expectedExceptions = NullPointerException.class
+    )
+    public void getFirstUnsupportedProcessIndexNullTest() {
+        MachineDistributor md = new MachineDistributor(16);
+        md.getFirstUnsupportedProcessIndex(null);
+    }
+
+    @Test(
+            expectedExceptions = IllegalArgumentException.class,
+            expectedExceptionsMessageRegExp = "Given RAM amount is negative"
+    )
+    public void checkGivenNegativeRamValuesTest() {
+        MachineDistributor md = new MachineDistributor(16);
+        md.getFirstUnsupportedProcessIndex(new int[] {Integer.MIN_VALUE});
+    }
+
+    // input, expected
+    @DataProvider(name = "data-provider-getFirstUnsupportedProcessIndex")
+    public Object[][] dataProviderGetFirstUnsupportedProcessIndex() {
+        return new Object[][] {
+                { new int[] {}, -1 },
+                { new int[] {16}, -1 },
+                { new int[] {2, 4, 6, 5, 8, 14, 16}, -1 },
+                { new int[] {Integer.MAX_VALUE}, 0 },
+                { new int[] {2, 4, 6, 5, 8, 14, 16, Integer.MAX_VALUE}, 7 },
+                { new int[] {Integer.MAX_VALUE, Integer.MAX_VALUE}, 0 }
+        };
     }
 }
