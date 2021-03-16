6b6cdce6199eb78a23cb9416aa632ea02261424e
Dmitry <dfedorino@gmail.com>
2021-03-16 22:08:28 +0800

    Add task implementation method and tests

--- src/main/java/com/dfedorino/rtasks/first_task/MachineDistributor.java
+++ src/main/java/com/dfedorino/rtasks/first_task/MachineDistributor.java
@@ -1,5 +1,6 @@
 package com.dfedorino.rtasks.first_task;
 
+import java.util.Arrays;
 import java.util.Objects;
 
 public class MachineDistributor {
@@ -10,11 +11,51 @@ public class MachineDistributor {
     }
 
     /**
+     * Algorithm is designed to return the amount of fully loaded machines with ramPerMachine memory
+     * available. The machine can run only 2 task simultaneously, so it is considered to be fully loaded
+     * in the following cases:
+     * 1. any task in the processes array consumes the ramPerMachine amount of memory;
+     * 2. any two tasks consume the ramPerMachine amount of memory in total;
+     * @param processes - array that represents processes' RAM consumption
+     * @throws IllegalArgumentException if any of the processes' value is negative
+     * @throws NullPointerException if the given array is null
+     * @return number of the fully loaded machines
+     */
+    public int getFullyLoadedMachines(int[] processes) {
+        Objects.requireNonNull(processes);
+        int[] theProcesses = new int[processes.length];
+        System.arraycopy(processes, 0, theProcesses, 0, processes.length);
+        Arrays.sort(theProcesses);
+        int lastSupportedProcessIndex = getFirstUnsupportedProcessIndex(theProcesses) == -1
+                ? theProcesses.length - 1
+                : getFirstUnsupportedProcessIndex(theProcesses) - 1;
+        int[] supportedProcesses = new int[lastSupportedProcessIndex + 1];
+        System.arraycopy(theProcesses, 0, supportedProcesses, 0, lastSupportedProcessIndex + 1);
+        int fullyUsedMachines = 0;
+        for (int processIndex = lastSupportedProcessIndex; processIndex >= 0; processIndex--) {
+            int currentProcess = supportedProcesses[processIndex];
+            int ramRemainder = ramPerMachine - currentProcess;
+            int ramRemainderIndex = Arrays.binarySearch(supportedProcesses, ramRemainder);
+            boolean processEqualsRam = ramRemainder == 0;
+            boolean twoProcessesEqualRam = ramRemainderIndex >= 0;
+            if (processEqualsRam || twoProcessesEqualRam) {
+                fullyUsedMachines++;
+                supportedProcesses[processIndex] = 0;
+                if (ramRemainderIndex >= 0) {
+                    supportedProcesses[ramRemainderIndex] = 0;
+                }
+            }
+        }
+        return fullyUsedMachines;
+    }
+
+    /**
      * Searches for the first element that is larger than the ramPerMachine value to exclude processes
      * with unsupported sizes from the search range.
+     *
      * @param sortedProcessArray - SORTED array with process ram consumption values
-     * @throws IllegalArgumentException if null is passed or if the given array contains negative values
      * @return the first index of the unsupported element, otherwise -1
+     * @throws IllegalArgumentException if null is passed or if the given array contains negative values
      */
     protected int getFirstUnsupportedProcessIndex(int[] sortedProcessArray) {
         Objects.requireNonNull(sortedProcessArray);
@@ -28,9 +69,10 @@ public class MachineDistributor {
 
     /**
      * Simple RAM value check
+     *
      * @param ramPerMachine - any int value that is supposed to represent RAM amount
-     * @throws IllegalArgumentException when the given value does not satisfy conditions of a valid RAM amount
      * @return argument value
+     * @throws IllegalArgumentException when the given value does not satisfy conditions of a valid RAM amount
      */
     private int checkRam(int ramPerMachine) {
         if (ramPerMachine < 0) {
--- src/test/java/com/dfedorino/rtasks/first_task/MachineDistributorTest.java
+++ src/test/java/com/dfedorino/rtasks/first_task/MachineDistributorTest.java
@@ -7,6 +7,24 @@ import static org.testng.Assert.assertSame;
 import static org.testng.Assert.assertEquals;
 
 public class MachineDistributorTest {
+    @Test(dataProvider = "data-provider-getFullyLoadedMachines")
+    public void getFullyLoadedMachinesTest(int ramPerMachine, int[] processesRam, int expectedMachineNumber) {
+        MachineDistributor md = new MachineDistributor(ramPerMachine);
+        int actualMachineNumber = md.getFullyLoadedMachines(processesRam);
+        assertEquals(actualMachineNumber, expectedMachineNumber);
+    }
+
+    @Test(
+            expectedExceptions = IllegalArgumentException.class,
+            expectedExceptionsMessageRegExp = "Given RAM amount is negative"
+    )
+    public void getFullyLoadedMachinesTest() {
+        MachineDistributor md = new MachineDistributor(16);
+        md.getFullyLoadedMachines(new int[] {Integer.MIN_VALUE});
+    }
+
+
+
     @Test
     public void constructorTest() {
         MachineDistributor md = new MachineDistributor(16);
@@ -33,9 +51,7 @@ public class MachineDistributorTest {
         MachineDistributor md1 = new MachineDistributor(Integer.MIN_VALUE);
     }
 
-    @Test(
-            expectedExceptions = NullPointerException.class
-    )
+    @Test(expectedExceptions = NullPointerException.class)
     public void getFirstUnsupportedProcessIndexNullTest() {
         MachineDistributor md = new MachineDistributor(16);
         md.getFirstUnsupportedProcessIndex(null);
@@ -60,6 +76,26 @@ public class MachineDistributorTest {
                 { new int[] {Integer.MAX_VALUE}, 0 },
                 { new int[] {2, 4, 6, 5, 8, 14, 16, Integer.MAX_VALUE}, 7 },
                 { new int[] {Integer.MAX_VALUE, Integer.MAX_VALUE}, 0 }
+        };
+    }
+
+    // input, expected
+    @DataProvider(name = "data-provider-getFullyLoadedMachines")
+    public Object[][] dataProviderGetFullyLoadedMachines() {
+        return new Object[][] {
+                { 16, new int[] {14, 5, 6, 2, 4, 16, 8}, 2 },
+                { 16, new int[] {16, 16, 16, 16}, 4 },
+                { 16, new int[] {0, 0, 0, 0}, 0 },
+                { 16, new int[] {0, 1, 2, 3, Integer.MAX_VALUE}, 0 },
+                { 16, new int[] {Integer.MAX_VALUE, 0, 1, 2, 3}, 0 },
+                { 16, new int[] {2, 14, 14, 14}, 1 },
+                { 16, new int[] {14, 14, 14, 2}, 1 },
+                { 16, new int[] {Integer.MAX_VALUE, 14, 5, 6, 2, 4, 16, 8}, 2 },
+                { 16, new int[] {Integer.MAX_VALUE, Integer.MAX_VALUE, 16}, 1 },
+                { 16, new int[] {16, Integer.MAX_VALUE, Integer.MAX_VALUE}, 1 },
+                { Integer.MAX_VALUE, new int[] {Integer.MAX_VALUE, Integer.MAX_VALUE}, 2 },
+                { Integer.MAX_VALUE, new int[] {16, Integer.MAX_VALUE, Integer.MAX_VALUE}, 2 },
+                { Integer.MAX_VALUE, new int[] {Integer.MAX_VALUE, Integer.MAX_VALUE, 16}, 2 }
         };
     }
 }
