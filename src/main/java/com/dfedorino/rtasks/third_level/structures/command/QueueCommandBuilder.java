eb4d4cab94d8d3d49c233c88b82a3ad0efc02e99
Dmitry <dfedorino@gmail.com>
2021-07-03 21:41:00 +0800

    Substitute CommandFactory with CommandBuilder

---
+++ src/main/java/com/dfedorino/rtasks/third_level/structures/command/QueueCommandBuilder.java
@@ -0,0 +1,35 @@
+package com.dfedorino.rtasks.third_level.structures.command;
+
+import java.util.List;
+import java.util.Queue;
+
+public class QueueCommandBuilder extends BaseCommandBuilder {
+    private final Queue<Integer> queue;
+
+    public QueueCommandBuilder(List<String> commands, Queue<Integer> queue) {
+        super(commands);
+        this.queue = queue;
+    }
+
+    @Override
+    public Command buildCommand(String commandString) {
+        if (commandString.startsWith("push")) {
+            return pushCommand(commandString, queue::offer);
+        } else if (commandString.equals("pop")) {
+            return () -> queue.isEmpty() ? new CommandResult("error") : new CommandResult(String.valueOf(queue.poll()));
+        } else if (commandString.equals("front")) {
+            return () -> queue.isEmpty() ? new CommandResult("error") : new CommandResult(String.valueOf(queue.peek()));
+        } else if (commandString.equals("size")) {
+            return command(queue::size);
+        } else if (commandString.equals("clear")) {
+            return () -> {
+                queue.clear();
+                return new CommandResult("ok");
+            };
+        } else if (commandString.equals("exit")) {
+            return () -> new CommandResult("bye");
+        } else {
+            throw new UnsupportedOperationException();
+        }
+    }
+}
--- src/test/java/com/dfedorino/rtasks/third_level/structures/command/ScriptExecutorTest.java
+++ src/test/java/com/dfedorino/rtasks/third_level/structures/command/ScriptExecutorTest.java
@@ -7,7 +7,6 @@ import java.util.Deque;
 import java.util.List;
 import java.util.Queue;
 import java.util.Stack;
-import java.util.stream.Collectors;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -30,9 +29,8 @@ public class ScriptExecutorTest {
                 "size",
                 "exit"
         );
-        List<Command> stackCommands = script.stream()
-                .map(commandString -> CommandFactory.createCommand(stack, commandString))
-                .collect(Collectors.toList()); // commands
+        CommandBuilder commandBuilder = new StackCommandBuilder(script, stack);
+        List<Command> stackCommands = commandBuilder.buildCommands(); // commands
         ScriptExecutor stackScriptExecutor = new ScriptExecutor(stackCommands); // sender
         List<String> actualProtocol = stackScriptExecutor.generateProtocol();
         List<String> expectedProtocol = List.of(
@@ -55,48 +53,6 @@ public class ScriptExecutorTest {
     }
 
     @Test
-    public void testGenerateProtocol_whenStackScript1_thenExpectedProtocol() {
-        List<String> script = List.of(
-                "push 3",
-                "push 14",
-                "size",
-                "clear",
-                "push 1",
-                "back",
-                "push 2",
-                "back",
-                "pop",
-                "size",
-                "pop",
-                "size",
-                "exit"
-        );
-        List<String> expectedProtocol = List.of(
-                "ok",
-                "ok",
-                "2",
-                "ok",
-                "ok",
-                "1",
-                "ok",
-                "2",
-                "2",
-                "1",
-                "1",
-                "0",
-                "bye"
-
-        );
-        Stack<Integer> stack = new Stack<>(); // получатель
-        List<Command> commands = script.stream()
-                .map(commandString -> CommandFactory.createCommand(stack, commandString))
-                .collect(Collectors.toList()); // команды
-        ScriptExecutor stackScriptExecutor = new ScriptExecutor(commands); // отправитель
-        List<String> actualProtocol = stackScriptExecutor.generateProtocol();
-        assertThat(actualProtocol).isEqualTo(expectedProtocol);
-    }
-
-    @Test
     public void testGenerateProtocol_whenQueueScript_thenExpectedProtocol() {
         Queue<Integer> queue = new ArrayDeque<>(); // receiver
         List<String> script = List.of(
@@ -104,9 +60,8 @@ public class ScriptExecutorTest {
                 "front",
                 "exit"
         );
-        List<Command> commands = script.stream()
-                .map(commandString -> CommandFactory.createCommand(queue, commandString))
-                .collect(Collectors.toList()); // commands
+        CommandBuilder commandBuilder = new QueueCommandBuilder(script, queue);
+        List<Command> commands = commandBuilder.buildCommands(); // commands
         ScriptExecutor queueScriptExecutor = new ScriptExecutor(commands); // sender
         List<String> actualProtocol = queueScriptExecutor.generateProtocol();
         List<String> expectedProtocol = List.of(
@@ -130,9 +85,8 @@ public class ScriptExecutorTest {
                 "size",
                 "exit"
         );
-        List<Command> commands = script.stream()
-                .map(commandString -> CommandFactory.createCommand(deque, commandString))
-                .collect(Collectors.toList()); // commands
+        CommandBuilder commandBuilder = new DequeCommandBuilder(script, deque);
+        List<Command> commands = commandBuilder.buildCommands(); // commands
         ScriptExecutor dequeScriptExecutor = new ScriptExecutor(commands); // sender
         List<String> actualProtocol = dequeScriptExecutor.generateProtocol();
         List<String> expectedProtocol = List.of(
