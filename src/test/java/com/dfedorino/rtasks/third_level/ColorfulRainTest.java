f284a9949c8dcce477073803e75efc12b78c87b7
Dmitry <dfedorino@gmail.com>
2021-07-15 22:25:22 +0800

    feat: solve colorful rain problem

---
+++ src/test/java/com/dfedorino/rtasks/third_level/ColorfulRainTest.java
@@ -0,0 +1,24 @@
+package com.dfedorino.rtasks.third_level;
+
+import org.testng.annotations.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class ColorfulRainTest {
+    private final ColorfulRain app = new ColorfulRain();
+
+    @Test
+    public void testCountBadBridges_whenGivenAdjacencyMatrix_thenNumberOfBadBridges() {
+        int[][] hills = {
+                {0, 1, 0, 0, 0, 1, 1},
+                {1, 0, 1, 0, 0, 0, 0},
+                {0, 1, 0, 0, 1, 1, 0},
+                {0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 1, 0, 0, 1, 0},
+                {1, 0, 1, 0, 1, 0, 0},
+                {1, 0, 0, 0, 0, 0, 0}
+        };
+        int[] hillsColors = {1, 1, 1, 1, 1, 3, 3};
+        assertThat(app.countBadBridges(hills, hillsColors)).isEqualTo(4);
+    }
+}
