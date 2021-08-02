ea32b438abfbf25f4b4b620b50581ac33799dc30
Dmitry <dfedorino@gmail.com>
2021-08-02 21:41:04 +0800

    feat: implement test for several components graph

--- src/test/java/com/dfedorino/rtasks/third_level/graphs/BreadthFirstSearchTest.java
+++ src/test/java/com/dfedorino/rtasks/third_level/graphs/BreadthFirstSearchTest.java
@@ -46,4 +46,23 @@ public class BreadthFirstSearchTest {
         // 4 -> 1, 2, 3, 4
         assertThat(app.traverse(adjacencyList)).isEqualTo(List.of(1, 2, 3, 4, 5));
     }
+
+    @Test
+    public void testTraverse_whenSeveralComponents_thenAllVertexesTraversed() {
+        List<List<Integer>> adjacencyList = List.of(
+                List.of(2, 3),
+                List.of(1, 3),
+                List.of(1, 2),
+                List.of(5),
+                List.of(4),
+                Collections.emptyList()
+        );
+        // 1 -> 2, 3
+        // 2 -> 1, 3
+        // 3 -> 1, 2
+        // 4 -> 5
+        // 5 -> 4
+        // 6
+        assertThat(app.traverse(adjacencyList)).isEqualTo(List.of(1, 2, 3, 4, 5, 6));
+    }
 }
