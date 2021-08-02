34bf8bafbb27768c0bb2df4f47196dca28ef0454
Dmitry <dfedorino@gmail.com>
2021-08-02 23:26:03 +0800

    fix: refactor names

--- src/main/java/com/dfedorino/rtasks/third_level/graphs/DepthFirstSearch.java
+++ src/main/java/com/dfedorino/rtasks/third_level/graphs/DepthFirstSearch.java
@@ -10,24 +10,24 @@ public class DepthFirstSearch implements Search{
         List<Integer> traversed = new ArrayList<>();
         for (int vertex = 1; vertex <= adjacencyList.size(); vertex++) {
             if (!was[vertex - 1]) {
-                traversed.addAll(getTraversedVertexes(vertex, was, adjacencyList));
+                traversed.addAll(getVertexes(vertex, was, adjacencyList));
             }
         }
         return traversed;
     }
 
-    private List<Integer> getTraversedVertexes(int vertex, boolean[] was, List<List<Integer>> adjacencyList) {
-        List<Integer> adjacent = adjacencyList.get(vertex - 1);
-        if (adjacent.isEmpty()) {
+    private List<Integer> getVertexes(int vertex, boolean[] was, List<List<Integer>> adjacencyList) {
+        List<Integer> adjacentVertexes = adjacencyList.get(vertex - 1);
+        if (adjacentVertexes.isEmpty()) {
             was[vertex - 1] = true;
             return List.of(vertex);
         } else {
             was[vertex - 1] = true;
             List<Integer> currentAndAdjacent = new ArrayList<>();
             currentAndAdjacent.add(vertex);
-            for (int adj : adjacent) {
-                if (!was[adj - 1]) {
-                    List<Integer> adjacentToAdjacent = getTraversedVertexes(adj, was, adjacencyList);
+            for (int adjacentVertex : adjacentVertexes) {
+                if (!was[adjacentVertex - 1]) {
+                    List<Integer> adjacentToAdjacent = getVertexes(adjacentVertex, was, adjacencyList);
                     currentAndAdjacent.addAll(adjacentToAdjacent);
                 }
             }
