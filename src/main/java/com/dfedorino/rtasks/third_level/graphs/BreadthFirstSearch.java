d74c7952876fcce4ca85add785aebcf5cd4074af
Dmitry <dfedorino@gmail.com>
2021-08-02 21:25:55 +0800

    feat: implement bfs

--- src/main/java/com/dfedorino/rtasks/third_level/graphs/BreadthFirstSearch.java
+++ src/main/java/com/dfedorino/rtasks/third_level/graphs/BreadthFirstSearch.java
@@ -1,11 +1,28 @@
 package com.dfedorino.rtasks.third_level.graphs;
 
+import java.util.ArrayDeque;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Queue;
 
 class BreadthFirstSearch implements Search {
 
     @Override
     public List<Integer> traverse(List<List<Integer>> adjacencyList) {
-        return null;
+        Queue<Integer> toBeVisited = new ArrayDeque<>(adjacencyList.size());
+        boolean[] was = new boolean[adjacencyList.size()];
+        List<Integer> visited = new ArrayList<>(adjacencyList.size());
+        toBeVisited.offer(1);
+        while(!toBeVisited.isEmpty()) {
+            int currentVertex = toBeVisited.poll();
+            if (!was[currentVertex - 1]) {
+                adjacencyList.get(currentVertex - 1).stream()
+                        .filter(vertex -> !was[vertex - 1])
+                        .forEach(toBeVisited::offer);
+                was[currentVertex - 1] = true;
+                visited.add(currentVertex);
+            }
+        }
+        return visited;
     }
 }
--- src/test/java/com/dfedorino/rtasks/third_level/graphs/BreadthFirstSearchTest.java
+++ src/test/java/com/dfedorino/rtasks/third_level/graphs/BreadthFirstSearchTest.java
@@ -2,6 +2,7 @@ package com.dfedorino.rtasks.third_level.graphs;
 
 import org.testng.annotations.Test;
 
+import java.util.Collections;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -10,12 +11,18 @@ public class BreadthFirstSearchTest {
     Search app = new BreadthFirstSearch();
 
     @Test
-    public void testTraverse__whenGivenAdjacencyList_thenAllAdjacentFirstThenTheirAdjacent() {
+    public void testTraverse_whenGivenAdjacencyList_thenAllAdjacentFirstThenTheirAdjacent() {
         List<List<Integer>> adjacencyList = List.of(
                 List.of(2, 3, 4),
                 List.of(5, 6),
                 List.of(7, 8, 9),
-                List.of(10)
+                List.of(10),
+                Collections.emptyList(),
+                Collections.emptyList(),
+                Collections.emptyList(),
+                Collections.emptyList(),
+                Collections.emptyList(),
+                Collections.emptyList()
         );
         // 1 -> 2, 3, 4
         // 2 -> 5, 6
