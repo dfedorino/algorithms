f9993e6ccbaaa13e704e9ae81384f6f96389edd5
Dmitry <dfedorino@gmail.com>
2021-08-04 10:58:48 +0800

    feat: implement postorder dfs

---
+++ src/main/java/com/dfedorino/rtasks/third_level/graphs/PostorderDfs.java
@@ -0,0 +1,29 @@
+package com.dfedorino.rtasks.third_level.graphs;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PostorderDfs implements Search {
+    @Override
+    public List<Integer> getTraversedVertexes(List<List<Integer>> adjacencyList) {
+        boolean[] was = new boolean[adjacencyList.size()];
+        List<Integer> visited = new ArrayList<>();
+        for (int vertex = 1; vertex < was.length; vertex++) {
+            if(!was[vertex]) {
+                traverseVertex(vertex, was, adjacencyList, visited);
+            }
+        }
+        return visited;
+    }
+
+    private void traverseVertex(int vertex, boolean[] was, List<List<Integer>> adjacencyList, List<Integer> visited) {
+        was[vertex] = true;
+        for (int adjacentVertex : adjacencyList.get(vertex)) {
+            if (!was[adjacentVertex]) {
+                traverseVertex(adjacentVertex, was, adjacencyList, visited);
+            }
+        }
+        // process vertex AFTER (post) all its adjacent vertexes
+        visited.add(vertex);
+    }
+}
---
+++ src/test/java/com/dfedorino/rtasks/third_level/graphs/PostorderDfsTest.java
@@ -0,0 +1,65 @@
+package com.dfedorino.rtasks.third_level.graphs;
+
+import org.testng.annotations.Test;
+
+import java.util.Collections;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class PostorderDfsTest {
+    private final Search postorderDfs = new PostorderDfs();
+
+    @Test
+    public void testGetTraversedVertexes_whenBinaryTree_thenPostorderTraversal() {
+        List<List<Integer>> binaryTree = List.of(
+                Collections.emptyList(), // index starts from 1
+                List.of(2, 3),
+                List.of(4, 5),
+                List.of(6, 7),
+                Collections.emptyList(),
+                Collections.emptyList(),
+                Collections.emptyList(),
+                Collections.emptyList()
+        );
+        assertThat(postorderDfs.getTraversedVertexes(binaryTree)).isEqualTo(List.of(4, 5, 2, 6, 7, 3, 1));
+    }
+
+    @Test
+    public void testGetTraversedVertexes_whenUndirectedConnectedGraph_thenPostorderTraversal() {
+        List<List<Integer>> undirectedConnectedGraph = List.of(
+                Collections.emptyList(), // index starts from 1
+                List.of(2, 3, 4),
+                List.of(5, 6),
+                List.of(7),
+                List.of(8, 9, 10),
+                Collections.emptyList(),
+                Collections.emptyList(),
+                Collections.emptyList(),
+                Collections.emptyList(),
+                Collections.emptyList(),
+                Collections.emptyList()
+        );
+        assertThat(postorderDfs.getTraversedVertexes(undirectedConnectedGraph))
+                .isEqualTo(List.of(5, 6, 2, 7, 3, 8, 9, 10, 4, 1));
+    }
+
+    @Test
+    public void testGetTraversedVertexes_whenUndirectedDisconnectedGraph_thenPostorderTraversal() {
+        List<List<Integer>> undirectedConnectedGraph = List.of(
+                Collections.emptyList(), // index starts from 1
+                List.of(2, 3),
+                List.of(5, 6),
+                List.of(7),
+                List.of(8, 9, 10),
+                Collections.emptyList(),
+                Collections.emptyList(),
+                Collections.emptyList(),
+                Collections.emptyList(),
+                Collections.emptyList(),
+                Collections.emptyList()
+        );
+        assertThat(postorderDfs.getTraversedVertexes(undirectedConnectedGraph))
+                .isEqualTo(List.of(5, 6, 2, 7, 3, 1, 8, 9, 10, 4));
+    }
+}
