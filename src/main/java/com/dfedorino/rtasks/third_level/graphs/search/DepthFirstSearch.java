c77a22338cc2aa1fdb02b15aad3b5021af0d61ef
Dmitry <dfedorino@gmail.com>
2021-08-10 21:55:21 +0800

    Refactor: split non-recursive dfs method into two

--- src/main/java/com/dfedorino/rtasks/third_level/graphs/search/DepthFirstSearch.java
+++ src/main/java/com/dfedorino/rtasks/third_level/graphs/search/DepthFirstSearch.java
@@ -8,29 +8,28 @@ public class DepthFirstSearch implements Search {
     @Override
     public List<Integer> getTraversedVertexes(List<List<Integer>> adjacencyList) {
         List<Integer> visited = new ArrayList<>(adjacencyList.size());
-        ArrayDeque<Integer> toBeVisited = new ArrayDeque<>(adjacencyList.size());
         boolean[] was = new boolean[adjacencyList.size()];
-        // for-loop in case there are several components in the graph
         for (int vertex = 1; vertex < was.length; vertex++) {
             if (!was[vertex]) {
-                toBeVisited.push(vertex);
-            }
-            while (!toBeVisited.isEmpty()) {
-                int currentVertex = toBeVisited.pop();
-                was[currentVertex] = true;
-                visited.add(currentVertex);
-                List<Integer> adjacentVertexes = adjacencyList.get(currentVertex);
-                for (int adjacentVertex = adjacentVertexes.size() - 1; adjacentVertex >= 0; adjacentVertex--) {
-                    int currentAdjacentVertex = adjacentVertexes.get(adjacentVertex);
-                    if (!was[currentAdjacentVertex]) {
-                        toBeVisited.push(currentAdjacentVertex);
-                        // this assignment ensures that a vertex that is already added to the toBeVisited
-                        // won't be added again
-                        was[currentAdjacentVertex] = true;
-                    }
-                }
+                traverseGraphStartingFrom(vertex, adjacencyList, was, visited);
             }
         }
         return visited;
+    }
+
+    private void traverseGraphStartingFrom(int vertex, List<List<Integer>> adjacencyList, boolean[] was, List<Integer> visited) {
+        ArrayDeque<Integer> toBeVisited = new ArrayDeque<>();
+        toBeVisited.push(vertex);
+        while (!toBeVisited.isEmpty()) {
+            int currentVertex = toBeVisited.pop();
+            if (!was[currentVertex]) {
+                was[currentVertex] = true;
+                visited.add(currentVertex);
+            }
+            List<Integer> adjacentVertexes = adjacencyList.get(currentVertex);
+            adjacentVertexes.stream()
+                    .filter(adjacentVertex -> !was[adjacentVertex])
+                    .forEach(toBeVisited::push);
+        }
     }
 }
