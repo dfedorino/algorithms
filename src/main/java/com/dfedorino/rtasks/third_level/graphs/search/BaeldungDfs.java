4fd19d36fc34eed81ac34d5c500c56cce32cec4b
Dmitry <dfedorino@gmail.com>
2021-08-10 19:47:35 +0800

    Fix: implement multiple components processing

--- src/main/java/com/dfedorino/rtasks/third_level/graphs/search/BaeldungDfs.java
+++ src/main/java/com/dfedorino/rtasks/third_level/graphs/search/BaeldungDfs.java
@@ -8,25 +8,29 @@ public class BaeldungDfs implements Search {
     @Override
     public List<Integer> getTraversedVertexes(List<List<Integer>> adjacencyList) {
         List<Integer> visited = new ArrayList<>(adjacencyList.size());
-        dfsWithoutRecursion(1, adjacencyList, visited);
+        dfsWithoutRecursion(adjacencyList, visited);
         return visited;
     }
 
-    // doesn't traverse multiple components,
-    public void dfsWithoutRecursion(int start, List<List<Integer>> adjVertices, List<Integer> visited) {
+    public void dfsWithoutRecursion(List<List<Integer>> adjVertices, List<Integer> visited) {
         Stack<Integer> stack = new Stack<>();
         boolean[] isVisited = new boolean[adjVertices.size()];
-        stack.push(start);
-        while (!stack.isEmpty()) {
-            int current = stack.pop();
-            if (!isVisited[current]) {
-                visited.add(current); // instead of 'visit(current);'
-                isVisited[current] = true;
-            }
-            for (int dest : adjVertices.get(current)) {
-                if (!isVisited[dest])
-                    stack.push(dest);
+        for (int vertex = 1; vertex < adjVertices.size(); vertex++) {
+            if (!isVisited[vertex]) {
+                stack.push(vertex);
+                while (!stack.isEmpty()) {
+                    int current = stack.pop();
+                    if (!isVisited[current]) {
+                        visited.add(current); // instead of 'visit(current);'
+                        isVisited[current] = true;
+                    }
+                    for (int dest : adjVertices.get(current)) {
+                        if (!isVisited[dest])
+                            stack.push(dest);
+                    }
+                }
             }
         }
+
     }
 }
