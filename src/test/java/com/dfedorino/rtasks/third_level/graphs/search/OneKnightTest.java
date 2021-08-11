ee717e7a23fe54c134c8b98bd3b02d5f2a2ccbc8
Dmitry <dfedorino@gmail.com>
2021-08-12 00:03:16 +0800

    Feat: implement OneKnight solution

---
+++ src/test/java/com/dfedorino/rtasks/third_level/graphs/search/OneKnightTest.java
@@ -0,0 +1,22 @@
+package com.dfedorino.rtasks.third_level.graphs.search;
+
+import com.dfedorino.rtasks.third_level.graphs.Pair;
+import org.testng.annotations.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class OneKnightTest {
+    private final OneKnight app = new OneKnight();
+    @Test
+    public void testGetShortestRoute() {
+        int fieldSize = 5;
+        Pair<Integer, Integer> start = new Pair<>(3, 3);
+        Pair<Integer, Integer> finish = new Pair<>(5, 1);
+        List<Pair<Integer, Integer>> route = app.getShortestRoute(fieldSize, start, finish);
+        System.out.println(">> route:");
+        route.forEach(System.out::println);
+        assertThat(route).hasSize(5);
+    }
+}
