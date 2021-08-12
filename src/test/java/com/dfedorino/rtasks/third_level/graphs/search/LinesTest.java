3dda2ecc5c928464367765f99a9b189d16720636
Dmitry <dfedorino@gmail.com>
2021-08-12 23:57:18 +0800

    Feat: implement Lines solutions

--- src/test/java/com/dfedorino/rtasks/third_level/graphs/search/LinesTest.java
+++ src/test/java/com/dfedorino/rtasks/third_level/graphs/search/LinesTest.java
@@ -1,4 +1,51 @@
-import static org.testng.Assert.*;
+package com.dfedorino.rtasks.third_level.graphs.search;
+
+import org.testng.annotations.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class LinesTest {
-    
+    private final Lines app = new Lines();
+
+    @Test
+    public void testGetRoute_whenNoObstacles_thenRouteIsBuilt() {
+        String[][] map = {
+                {".", ".", "X"},
+                {".", "@", "."},
+                {".", ".", "."}
+        };
+        Lines.Route expectedRoute = new Lines.Route();
+        expectedRoute.setFound(true);
+        expectedRoute.setRoute(new String[][]{
+                {".", "+", "X"},
+                {".", "@", "."},
+                {".", ".", "."}
+        });
+        assertThat(app.getRoute(map)).isEqualTo(expectedRoute);
+    }
+
+    @Test
+    public void testGetRoute_whenTestCase1_thenRouteIsBuilt() {
+        String[][] map = {
+                {"@", "."},
+                {".", "X"}
+        };
+        Lines.Route expectedRoute = new Lines.Route();
+        expectedRoute.setFound(true);
+        expectedRoute.setRoute(new String[][]{
+                {"@", "+"},
+                {".", "X"}
+        });
+        assertThat(app.getRoute(map)).isEqualTo(expectedRoute);
+    }
+
+    @Test
+    public void testGetRoute_whenNoRoute_thenNoRouteIsBuilt() {
+        String[][] map = {
+                {"@", "O"},
+                {"O", "X"}
+        };
+        Lines.Route expectedRoute = new Lines.Route();
+        assertThat(app.getRoute(map)).isEqualTo(expectedRoute);
+    }
 }
